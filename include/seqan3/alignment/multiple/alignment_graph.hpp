de59b71d63e5c7ddec9d56b4b7477233ef6b85c0
Jörg Winkler <j.winkler@fu-berlin.de>
2019-07-15 17:03:31 +0200

    refine constructors and modernise code

--- include/seqan3/alignment/multiple/alignment_graph.hpp
+++ include/seqan3/alignment/multiple/alignment_graph.hpp
@@ -2,99 +2,124 @@
 // Copyright (c) 2006-2019, Knut Reinert & Freie Universität Berlin
 // Copyright (c) 2016-2019, Knut Reinert & MPI für molekulare Genetik
 // This file may be used, modified and/or redistributed under the terms of the 3-clause BSD-License
-// shipped with this file and also available at: https://github.com/seqan/seqan3/blob/master/LICENSE
+// shipped with this file and also available at: https://github.com/seqan/seqan3/blob/master/LICENSE.md
 // -----------------------------------------------------------------------------------------------------
 
 /*!\file
- * \author Joerg Winkler <j.winkler AT fu-berlin.de>
+ * \author Jörg Winkler <j.winkler AT fu-berlin.de>
  * \brief Contains the alignment graph for (multiple) sequence alignments.
  */
 
 #pragma once
 
+#include <seqan3/core/concept/core_language.hpp>
 #include <seqan3/core/platform.hpp>
-#include <seqan3/range/concept.hpp>
-#include <seqan3/alphabet/concept.hpp>
+#include <seqan3/std/ranges>
 
 #if SEQAN3_WITH_LEMON
 
 #include <lemon/list_graph.h>
 
-namespace seqan3
+namespace seqan3::detail
 {
 
-template <std::ranges::RandomAccessRange sequence_container_type, typename weight_type = int>
-requires std::ranges::RandomAccessRange<typename sequence_container_type::value_type>
-      && std::ranges::SizedRange<typename sequence_container_type::value_type>
-      && Alphabet<typename sequence_container_type::value_type::value_type>
+template <Arithmetic weight_type = int>
 class alignment_graph
 {
-protected:
-    //                               seq_id    offset    length
-    using fragment_type = std::tuple<unsigned, unsigned, unsigned>;
-
+private:
     //! \brief The k-partite graph that stores the connections between fragments.
     lemon::ListDigraph graph;
 
     //! \brief The edges of the graph contain weights.
     lemon::ListDigraph::ArcMap<weight_type> weights;
 
-    //! \brief The underlying sequences.
-    sequence_container_type sequences;
+    //! \brief The number of nodes in the graph.
+    size_t num_nodes;
 
-
-    std::vector<fragment_type> fragments;
-    unsigned num_nodes;
+    //! \brief The number of sequences in the graph.
+    size_t num_seqs;
 
 public:
+    /*!\name Constructors, destructor and assignment
+     * \{
+     */
+
+    //! Default constructor.
+    constexpr alignment_graph() :
+        graph{},
+        weights{graph},
+        num_nodes{0},
+        num_seqs{0}
+    {}
+
     /*!
      * \brief Constructor for an alignment graph.
      * \param seq A sequence container that the graph is linked to.
      */
-    alignment_graph(sequence_container_type & seq) : weights(graph), sequences(seq), num_nodes(0u)
+    template <std::ranges::ForwardRange seqs_t>
+        requires std::ranges::SizedRange<typename seqs_t::value_type>
+    constexpr alignment_graph(seqs_t const & sequences) : alignment_graph()
     {
         // count the number of characters in all the sequences
-        for (auto sequence : sequences)
-            num_nodes += sequence.size();
+        for (auto const & sequence : sequences)
+        {
+            num_nodes += std::ranges::size(sequence);
+            ++num_seqs;
+        }
 
         // create fragments and nodes
-        fragments.resize(num_nodes);
         graph.reserveNode(num_nodes);
-        for (unsigned cnt = 0u; cnt < num_nodes; ++cnt)
+        for (size_t cnt = 0ul; cnt < num_nodes; ++cnt)
             graph.addNode();
     }
 
-    //! delete default constructor
-    alignment_graph() = delete;
-
-    //! copy constructor
-    alignment_graph(alignment_graph const & origin) : sequences(origin.sequences), fragments(origin.fragments)
+    //! Copy constructor.
+    constexpr alignment_graph(alignment_graph const & origin) :
+        graph{},
+        weights{graph},
+        num_nodes{origin.num_nodes},
+        num_seqs{origin.num_seqs}
     {
         lemon::digraphCopy(origin.graph, graph).arcMap(origin.weights, weights).run();
     }
 
-    //! copy assignment
-    alignment_graph & operator=(alignment_graph const & origin)
+    //! Copy assignment.
+    constexpr alignment_graph & operator=(alignment_graph const & origin)
     {
-        return alignment_graph(origin);
+        return alignment_graph{origin};
     }
 
-    //! default move constructor / assigment
-    alignment_graph(alignment_graph &&) = default;
-    alignment_graph & operator=(alignment_graph &&) = default;
+    //! Default move constructor.
+    constexpr alignment_graph(alignment_graph &&) noexcept = default;
+
+    //! Default move assignment.
+    constexpr alignment_graph & operator=(alignment_graph &&) noexcept = default;
+
+    //! Default destructor.
+    ~alignment_graph() noexcept = default;
+    //!\}
 
     /*!
      * \brief Return the number of nodes in the graph (constant time access).
      * \return number of nodes
      */
-    unsigned size()
+    constexpr size_t size() const noexcept
     {
         return num_nodes;
     }
+
+    /*!
+     * \brief Return the number of sequences in the graph (constant time access).
+     * \return number of sequences
+     */
+    constexpr size_t num_sequences() const noexcept
+    {
+        return num_seqs;
+    }
 };
 
-} // namespace seqan3
+} // namespace seqan3::detail
 
 #else
-    #error "This module is only available if the LEMON library is installed. See README for more information."
+    #error "This module is only available if the LEMON graph library is installed. See README for more information."
 #endif // SEQAN3_WITH_LEMON
--- test/unit/alignment/multiple/alignment_graph_test.cpp
+++ test/unit/alignment/multiple/alignment_graph_test.cpp
@@ -17,7 +17,7 @@
 #include <type_traits>
 #include <vector>
 
-#include <seqan3/alphabet/all.hpp>
+#include <seqan3/alphabet/nucleotide/rna5.hpp>
 #include <seqan3/alignment/multiple/alignment_graph.hpp>
 
 using namespace seqan3;
@@ -26,42 +26,45 @@ template <typename T>
 class align_graph : public ::testing::Test
 {};
 
-// ------------------------------------------------------------------
-// basics
-// ------------------------------------------------------------------
-
-std::vector<rna5_vector> sequences{"ACG"_rna5, "UCG"_rna5, "CCG"_rna5};
-
 TEST(align_graph, constructors)
 {
-    using TRandomAccessContainer = std::vector<rna5_vector>;
-    EXPECT_FALSE((std::is_default_constructible_v<alignment_graph<TRandomAccessContainer>>));
-    EXPECT_TRUE((std::is_copy_constructible_v<alignment_graph<TRandomAccessContainer>>));
-    EXPECT_TRUE((std::is_move_constructible_v<alignment_graph<TRandomAccessContainer>>));
-    EXPECT_TRUE((std::is_copy_assignable_v<alignment_graph<TRandomAccessContainer>>));
-    EXPECT_TRUE((std::is_move_assignable_v<alignment_graph<TRandomAccessContainer>>));
+    EXPECT_TRUE((std::is_default_constructible_v<detail::alignment_graph<>>));
+    EXPECT_TRUE((std::is_copy_constructible_v<detail::alignment_graph<>>));
+    EXPECT_TRUE((std::is_move_constructible_v<detail::alignment_graph<>>));
+    EXPECT_TRUE((std::is_copy_assignable_v<detail::alignment_graph<>>));
+    EXPECT_TRUE((std::is_move_assignable_v<detail::alignment_graph<>>));
+    EXPECT_TRUE((std::is_destructible_v<detail::alignment_graph<>>));
+}
+
+TEST(align_graph, default_constructor)
+{
+    detail::alignment_graph ag{};
+    EXPECT_EQ(ag.size(), 0ul);
 }
 
 TEST(align_graph, input_container)
 {
     // std::vector<std::vector<rna5>>
-    alignment_graph graph_vv{sequences};
+    std::vector<rna5_vector> seq_vec{"ACG"_rna5, "UCG"_rna5, "CCG"_rna5};
+    detail::alignment_graph graph_vv{seq_vec};
 
     // std::array of std::vector
-    std::array<dna15_vector, 3> seq_array{"ACG"_dna15, "TCG"_dna15, "CCG"_dna15};
-    alignment_graph graph_av{seq_array};
+    std::array<rna5_vector, 3> seq_array{"ACG"_rna5, "UCG"_rna5, "CCG"_rna5};
+    detail::alignment_graph graph_av{seq_array};
 
     // std::array of std::array
-    std::array<std::array<aa27, 10>, 10> seq_array2{};
-    alignment_graph graph_aa{seq_array2};
+    std::array<std::array<rna5, 10>, 10> seq_array2{};
+    detail::alignment_graph graph_aa{seq_array2};
 
     // std::deque of std::deque
-    std::deque<std::deque<wuss51>> seq_deque{};
-    alignment_graph graph_dd{seq_deque};
+    std::deque<std::deque<rna5>> seq_deque{};
+    detail::alignment_graph graph_dd{seq_deque};
 }
 
 TEST(align_graph, size)
 {
-    alignment_graph graph{sequences};
-    EXPECT_EQ(graph.size(), 9u);
+    std::vector<rna5_vector> sequences{"ACG"_rna5, "UCG"_rna5, "CCG"_rna5};
+    detail::alignment_graph graph{sequences};
+    EXPECT_EQ(graph.size(), 9ul);
+    EXPECT_EQ(graph.num_sequences(), 3ul);
 }
